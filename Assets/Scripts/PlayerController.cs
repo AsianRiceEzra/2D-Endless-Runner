6607fd19a12bfbb6d98b77360f36803cc35d6e6d
AsianRiceEzra <eestud2022@elam.co.uk>
2022-10-10 15:44:55 +0100

    EndlessRunner

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,13 +6,16 @@ public class PlayerController : MonoBehaviour
 {
     public GameObject groundChecker;
     public LayerMask whatIsGround;
+    public Animator anim;
     float maxSpeed = 15.0f;
     bool isOnGround = false;
+
+    
 
     Rigidbody2D playerObject;
 
    
-    Vector3 jumpForce = new Vector3(0.0f, 10.0f, 0.0f);
+    Vector3 jumpForce = new Vector3(0.0f, 15.0f, 0.0f);
 
     bool isJumping = true;
 
@@ -33,12 +36,21 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            maxSpeed = 10.0f;
+        }else
+        {
+            maxSpeed = 5.0f;
+        }
 
-        float movementValueX = Input.GetAxis("Horizontal");
+        float movementValueX = 2.0f;
+
+
 
         playerObject.velocity = new Vector2(movementValueX * maxSpeed, playerObject.velocity.y);
 
-        isOnGround = Physics2D.OverlapCircle(groundChecker.transform.position, 1.0f, whatIsGround);
+        isOnGround = Physics2D.OverlapCircle(groundChecker.transform.position, 2.0f, whatIsGround);
 
 
         if (Input.GetKeyDown(KeyCode.Space) && isOnGround == true)
@@ -46,5 +58,6 @@ public class PlayerController : MonoBehaviour
             playerObject.AddForce(new Vector2(0.0f, 500.0f));
         }
 
+        anim.SetBool("IsGrounded", isOnGround);
     }
 }
